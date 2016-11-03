412549ce036552ec1cc6d982ae15fce6cdb93101
tessereth <teresa.bradbury@digital.gov.au>
2016-11-04 10:56:04 +1100

    Add space list method

--- lib/cfoundry_client/spaces.rb
+++ lib/cfoundry_client/spaces.rb
@@ -1,5 +1,9 @@
 class CfoundryClient
   module Spaces
+    def spaces(params = {})
+      CfoundryClient::Paginator.new(self, "spaces", params)
+    end
+
     def space_apps(space_guid, params = {})
       CfoundryClient::Paginator.new(self, "spaces/#{space_guid}/apps", params)
     end
